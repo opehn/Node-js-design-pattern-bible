1a6a9c789f81d8066c9b4c44ab6fb46519d266f7
acho <opehn0@gmail.com>
2024-08-10 22:57:47 +0900

    [8] 안전한 계산기 만들기 - 객체 확장
    - Subject를 직접 수정하는 방법이다. 래퍼 함수를 다시 원래의 Subject에 할당한다.
    - 이 래퍼 함수가 프록시다.
    - 한 두개의 함수를 프록시할 때 확실히 편리하다. 나머지 함수들을 일일이 위임할 필요가 없기 때문이다.
      하지만, 대상 객체를 직접 변경하기 때문에 위험할 수 있다.
      만약 Subject가 다른 부분과 공유되어 있을 경우 무조건 피해야 하는 방법이다. 변화가 애플리케이션의 다른 컴포넌트에 영향을 미칠 수 있기 때문이다.
      Subject가 제한된 컨텍스트나 프라이빗 범위에 있는 경우에만 이 기술을 사용해야 한다.

---
+++ chapter8/patch-to-safe-calculator.js
@@ -0,0 +1,28 @@
+import { StackCalculator } from "./stack-calculator.js";
+
+function patchToSafeCalculator(calculator) {
+  const divideOrig = calculator.divide;
+  calculator.divide = () => {
+    //추가적인 검증 로직
+    const divisor = calculator.peekValue();
+    if (divisor === 0) {
+      throw Error("Division by 0");
+    }
+    //Subject에 유효한 위임자일 경우
+    return divideOrig.apply(calculator);
+  };
+
+  return calculator;
+}
+
+const calculator = new StackCalculator();
+const safeCalculator = patchToSafeCalculator(calculator);
+
+calculator.putValue(3);
+calculator.putValue(0);
+
+try {
+  console.log(safeCalculator.divide());
+} catch (e) {
+  console.log("Error: " + e.message);
+}
